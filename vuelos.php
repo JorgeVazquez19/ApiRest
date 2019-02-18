da6a8d6dbb6190b5d230bea44c36f341e7d1ba2b
JorgeVazquez <jorgitoovazquez@hotmail.com>
2019-02-18 12:32:05 +0100

    Comentado del todo

--- vuelos.php
+++ vuelos.php
@@ -1,10 +1,13 @@
 <?php
+//clase donde se encuentran las querys de peticion a la bbdd
 include_once 'database.php';
 class vuelos extends database{
+  //obtiene todos los vuelos
     function obtenerVuelos(){
         $query = $this->connect()->query('SELECT * FROM vuelos');
         return $query;
     }
+    //obtiene los vuelos con plazas libres
     function obtenerFree(){
         $query = $this->connect()->query('SELECT * FROM vuelos WHERE plazas_libres > 0');
         return $query;
